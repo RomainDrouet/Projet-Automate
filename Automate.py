76a05a601280e3a6eef29b89c7c21e87a573a5f9
Bl4ckfox99 <130778528+Bl4ckfox99@users.noreply.github.com>
2024-04-16 00:26:13 +0200

    Update Automate.py

--- Automate.py
+++ Automate.py
@@ -1,10 +1,46 @@
 class Automate:
 
-    def __init__(self):
+    def __init__(self, auto):
         self.automate = []
+        with open(auto, 'r') as fichier:
+            lignes = fichier.readlines()
+        self.nb_alphabet = int(lignes[0].strip())
+        self.nb_etat = int(lignes[1].strip())
+        self.etat = []
+        for i in range(self.nb_etat):
+            self.etat.append(i)
         self.alphabet = [" ", "Etat"]
-        self.entsor = ["E", " ", " ", "S", "S"]
-        self.etat = [0, 1, 2, 3, 4]
+        for i in range(self.nb_alphabet):
+            self.alphabet.append(chr(97+i))
+        liste_transitions = []
+        for i in range(7, len(lignes)):
+            transition = lignes[i].strip()
+            liste_transitions.append(transition)
+        ent = list(map(int, lignes[3].strip().split()))
+        sor = list(map(int, lignes[5].strip().split()))
+        entree = []
+        sortie = []
+        self.entsor = []
+        for i in range(len(self.etat)):
+            self.entsor.append(" ")
+        for i in range(len(self.etat)):
+            entree.append(" ")
+        for i in range(len(self.etat)):
+            sortie.append(" ")
+        for i in range(len(ent)):
+            entree[ent[i]] = "E"
+        for i in range(len(sor)):
+            sortie[sor[i]] = "S"
+        for i in range(len(self.etat)):
+            if sortie[i] == "S" and entree[i] == "E":
+                self.entsor[i] = "E/S"
+            elif sortie[i] == "S" :
+                self.entsor[i] = "S"
+            elif entree[i] == "E" :
+                self.entsor[i] = "E"
+        self.transition = liste_transitions
+        self.nb_transition = int(lignes[6].strip())
+
 
     def affichage(self):
         supg = '┌'
@@ -133,6 +169,3 @@ class Automate:
 
     def add_ligne(self, automate):
         self.automate.append(automate)
-
-    def add_alphabet(self, alphabet):
-        self.alphabet.append(alphabet)
