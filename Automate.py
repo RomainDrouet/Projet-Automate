ce2b2228b38c6645c8e99d35c68b4d5a5c0599c4
Bl4ckfox99 <130778528+Bl4ckfox99@users.noreply.github.com>
2024-04-16 20:59:58 +0200

    Update Automate.py

--- Automate.py
+++ Automate.py
@@ -8,7 +8,7 @@ class Automate:
         self.nb_etat = int(lignes[1].strip())
         self.etat = []
         for i in range(self.nb_etat):
-            self.etat.append(i)
+            self.etat.append(str(i))
         self.alphabet = [" ", "Etat"]
         for i in range(self.nb_alphabet):
             self.alphabet.append(chr(97+i))
@@ -34,13 +34,13 @@ class Automate:
         for i in range(len(self.etat)):
             if sortie[i] == "S" and entree[i] == "E":
                 self.entsor[i] = "E/S"
-            elif sortie[i] == "S" :
+            elif sortie[i] == "S":
                 self.entsor[i] = "S"
-            elif entree[i] == "E" :
+            elif entree[i] == "E":
                 self.entsor[i] = "E"
         self.transition = liste_transitions
         self.nb_transition = int(lignes[6].strip())
-
+        self.ent = ent
 
     def affichage(self):
         supg = '┌'
@@ -102,7 +102,7 @@ class Automate:
 
             for j in range(len(self.automate[i])):
                 compteur = 0
-                compteur +=  len(self.automate[i][j])-1
+                compteur += len(self.automate[i][j])-1
                 for k in range(len(self.automate[i][j])):
                     compteur += len(self.automate[i][j][k])
                 print(" " * (3-compteur//2), end="")
@@ -169,3 +169,77 @@ class Automate:
 
     def add_ligne(self, automate):
         self.automate.append(automate)
+
+    def est_standart(self):
+        return False
+
+    def standardisation(self):
+
+        """regarde si il faut faire un etat final et/ou initial"""
+        initial = True
+        final = True
+        for i in range(len(self.automate)):
+            for j in range(len(self.automate[i])):
+                for k in range(len(self.automate[i][j])):
+                    for ent in self.ent:
+                        if self.automate[i][j][k] == str(ent):
+                            initial = False
+                    if self.automate[len(self.automate)-1][j][k] != "-1":
+                        final = False
+
+        """fait un etat final et standardise"""
+        if not final:
+            self.etat.append("s")
+            self.automate.append([])
+            for i in range(self.nb_alphabet):
+                self.automate[len(self.automate) - 1].append(["-1"])
+            tmp = []
+            for i in range(len(self.entsor)):
+                if self.entsor[i] == "S" or self.entsor[i] == "E/S":
+                    tmp.append(i)
+            for i in range(len(tmp)):
+                for j in range(self.nb_alphabet):
+                    if self.automate[tmp[i]][j] == ["-1"]:
+                        self.automate[tmp[i]][j] = str(int(self.etat[len(self.etat) - 2]) + 1)
+                    else:
+                        self.automate[tmp[i]][j].append(str(int(self.etat[len(self.etat) - 2]) + 1))
+
+        """fait un etat inital et standardise"""
+        if not initial:
+            self.etat.insert(0, "i")
+            tmp = []
+            for i in range(len(self.entsor)):
+                if self.entsor[i] == "E" or self.entsor[i] == "E/S":
+                    tmp.append(str(i))
+            self.automate.insert(0, [])
+            for i in range(self.nb_alphabet):
+                self.automate[0].append(tmp)
+
+        if not final or not initial:
+            self.entsor = []
+            for i in range(len(self.etat)):
+                self.entsor.append(" ")
+            self.entsor[0] = "E"
+            self.entsor[len(self.entsor)-1] = "S"
+            self.retablir_etat()
+
+    def test1(self, tab):
+        self.entsor = tab
+
+    def retablir_etat(self):
+        self.automate = self.incrementer_liste(self.automate)
+        for i in range(len(self.etat)):
+            self.etat[i] = str(i)
+
+    def incrementer_liste(self, liste):
+        nouvelle_liste = []
+        for sous_liste in liste:
+            nouvelle_sous_liste = []
+            for valeur in sous_liste:
+                if valeur != ['-1']:
+                    nouvelle_valeur = [str(int(i) + 1) for i in valeur]
+                    nouvelle_sous_liste.append(nouvelle_valeur)
+                else:
+                    nouvelle_sous_liste.append(valeur)
+            nouvelle_liste.append(nouvelle_sous_liste)
+        return nouvelle_liste
