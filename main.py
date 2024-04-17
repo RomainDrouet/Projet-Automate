8907893047853c224fb38866afc5d4f61e801571
Bl4ckfox99 <130778528+Bl4ckfox99@users.noreply.github.com>
2024-04-17 20:00:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,27 +3,7 @@ from Automate import *
 
 auto7 = 'F7-6.txt'
 
-
-a = Automate(auto7)
-
-a.add_ligne([["0", "1"], ["0","3"]])
-a.add_ligne([["-1"], ["-1"]])
-a.add_ligne([["-1"], ["3"]])
-a.add_ligne([["3"], ["-1"]])
-
-
-"""a.affichage()"""
-
-
-
 b = Automate(auto7)
-
-b.add_ligne([["1"], ["2"]])
-b.add_ligne([["0"], ["-1"]])
-b.add_ligne([["-1"], ["2"]])
-b.add_ligne([["3"], ["-1"]])
-
-b.test1(["E"," "," ","S"])
 
 b.affichage()
 
