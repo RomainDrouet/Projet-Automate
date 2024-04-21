31167fc96179abb2710b58ed2613fca9862f4128
RomainDrouet <103487910+RomainDrouet@users.noreply.github.com>
2024-04-21 11:38:24 +0200

    mise à jour du fichier main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from Automate import *
 
-a = Automate()
 
-a.add_ligne([["0", "1"], ["0","4"], ["0"]])
-a.add_ligne([["-1"], ["-1"], ["2"]])
-a.add_ligne([["-1"], ["3"], ["-1"]])
-a.add_ligne([["3"], ["-1"], ["-1"]])
-a.add_ligne([["-1"], ["-1"], ["2"]])
+auto7 = 'F7-6.txt'
 
-a.add_alphabet("a")
-a.add_alphabet("b")
-a.add_alphabet("c")
+b = Automate(auto7)
 
-a.affichage()
+b.affichage()
 
-"""Matthieu"""
+b.standardisation()
+
+b.affichage()
